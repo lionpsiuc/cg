3b27676fba12f249660da1efe9e944aff6e8535c
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-04-16 10:59:11 +0100

    Remove testing functions

--- cg.c
+++ cg.c
@@ -17,71 +17,6 @@ double f(double x1, double x2) {
   return 2.0 * M_PI * M_PI * sin(M_PI * x1) * sin(M_PI * x2);
 }
 
-// Finite difference method
-void fd(double* A, int N) {
-  double h      = 1.0 / ((double) N + 1); // Grid spacing
-  double h2_inv = 1.0 / (h * h);
-  int    size   = N * N;
-
-  // Initialise all elements to zero
-  for (int i = 0; i < size * size; i++) {
-    A[i] = 0.0;
-  }
-
-  // Set the diagonal and off-diagonal elements
-  for (int j = 0; j < N; j++) {
-    for (int i = 0; i < N; i++) {
-      int index = j * N + i; // Needed for row-major ordering
-
-      // Diagonal element (i.e., coefficient of u(i,j))
-      A[index * size + index] = 4.0 * h2_inv;
-
-      // Connect to left neighbour, if not at the left edge
-      if (i > 0) {
-        A[index * size + (index - 1)] = -h2_inv;
-      }
-
-      // Connect to right neighbour, if not at the right edge
-      if (i < N - 1) {
-        A[index * size + (index + 1)] = -h2_inv;
-      }
-
-      // Connect to top neighbour, if not at the top edge
-      if (j > 0) {
-        A[index * size + (index - N)] = -h2_inv;
-      }
-
-      // Connect to bottom neighbour, if not at bottom edge
-      if (j < N - 1) {
-        A[index * size + (index + N)] = -h2_inv;
-      }
-    }
-  }
-
-  // Print the matrix
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      printf("%8.2f ", A[i * size + j]);
-    }
-    printf("\n");
-  }
-}
-
-// Matrix-vector multiplication
-void mvp(double* A, double* x, double* y, int N) {
-  int size = N * N;
-
-  // Initialise with zeros
-  for (int i = 0; i < size; i++) {
-    y[i] = 0.0;
-  }
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      y[i] += A[i * size + j] * x[j];
-    }
-  }
-}
-
 // Optimised method of calculting Ax; instead of storing A, we multiply directly
 // using this function
 void poisson(double* x, double* y, int N) {
@@ -130,34 +65,6 @@ void rhs(double* b, int N, double (*f)(double, double)) {
       b[index]     = f(x1, x2);   // Evaluate our function at this point
     }
   }
-}
-
-// Just testing to make sure my poisson function is correctly multiplying things
-// together; comparing it with an explicit matrix-vector multiplication using
-// mvp
-void test() {
-  int     N    = 3;
-  int     size = N * N;
-  double* A    = (double*) malloc(size * size * sizeof(double));
-  double* x    = (double*) malloc(size * sizeof(double));
-  double* y1   = (double*) malloc(size * sizeof(double));
-  double* y2   = (double*) malloc(size * sizeof(double));
-  fd(A, N);
-  for (int i = 0; i < size; i++) {
-    x[i] = 1.0;
-  }
-  mvp(A, x, y1, N);
-  poisson(x, y2, N);
-  for (int i = 0; i < size; i++) {
-    printf("%f\n", y1[i]);
-  }
-  for (int i = 0; i < size; i++) {
-    printf("%f\n", y2[i]);
-  }
-  free(A);
-  free(x);
-  free(y1);
-  free(y2);
 }
 
 // Write the computed solution to a file that can be used for plotting
